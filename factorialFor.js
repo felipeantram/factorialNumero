8e3e570e36213b9f6db6b96c8e39c532744ff04c
Antonio <felipeantram05@gmail.com>
2026-09-08 12:20:16 -0600

    eliminando funsion serie

--- factorialFor.js
+++ factorialFor.js
@@ -12,32 +12,3 @@ let num = 5;
 
 console.log("El factorial de " + num + " es: " + factorial(num));
 
-function factorial(numero) {
-    // Caso base
-    if (numero === 0 || numero === 1) {
-        return 1;
-    }
-
-    // Llamada recursiva
-    return numero * factorial(numero - 1);
-}
-
-// Número a calcular
-let nmro = 5;
-
-console.log("El factorial de " + nmr + " es: " + factorial(nmr));
-
-function factorial(numero) {
-    // Caso base
-    if (numero === 0 || numero === 1) {
-        return 1;
-    }
-
-    // Llamada recursiva
-    return numero * factorial(numero - 1);
-}
-
-// Número a calcular
-let nmr = 5;
-
-console.log("El factorial de " + nmr + " es: " + factorial(nmr));
