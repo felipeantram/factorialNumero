9ac80dc1233e8dfca7e0ebd5fd527bbb0fa58660
Antonio <felipeantram05@gmail.com>
2026-09-08 11:36:43 -0600

    nuevo bloque de codigo

--- factorialFor.js
+++ factorialFor.js
@@ -8,7 +8,21 @@ function factorial(numero) {
     return resultado;
 }
 
-let numero = 5;
+let num = 5;
 
 console.log("El factorial de " + numero + " es: " + factorial(numero));
 
+function factorial(numero) {
+    // Caso base
+    if (numero === 0 || numero === 1) {
+        return 1;
+    }
+
+    // Llamada recursiva
+    return numero * factorial(numero - 1);
+}
+
+// Número a calcular
+let numero = 5;
+
+console.log("El factorial de " + numero + " es: " + factorial(numero));
